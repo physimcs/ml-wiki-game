7dec6224a4d5bcf87aca94aa639f5aca676b6a64
Sonia Tyburczy <sonia@Sonias-MacBook-Pro.local>
2025-06-06 21:44:14 -0400

    get_links now returns title link dictionary instead of list of links

--- src/scraper.py
+++ src/scraper.py
@@ -4,22 +4,28 @@ import requests
 class Scraper:
     def __init__(self, current_link):
         self.parent_link = current_link
-        self.child_links = []
+        self.child_links = {}
 
-    def get_links(self):
+    def scrape_links(self):
         response = requests.get(self.parent_link)
         soup = BeautifulSoup(response.text, 'html')
 
         main_content = soup.find(id='mw-content-text')
         links = main_content.find_all('a', href=True)
 
-        internal_links = []
+        return links
+    
+    def get_links(self):
+        links = self.scrape_links()
+
+        internal_links = {}
         for link in links:
             href = link['href']
             # Check to filter out non-internal links + unwanted internal links
             if href.startswith('/wiki') and ':' not in href and 'Wiki' not in href:
                 url = 'https://en.wikipedia.org' + href
-                internal_links.append(url)
+                title = href[6::].replace('_', ' ')
+                internal_links[title] = url
 
         self.child_links = internal_links
         return self.child_links
