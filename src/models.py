7ff000aeb4090a8024a256e707f0626576684d57
Sonia Tyburczy <sonia@Sonias-MacBook-Pro.local>
2025-06-16 17:11:19 -0400

    Organized model as class

---
+++ src/models.py
@@ -0,0 +1,30 @@
+from sentence_transformers import SentenceTransformer, util
+
+class WikiModel:
+    def __init__(self, current_link, target_link, internal_link_pairs):
+        self.current_link = current_link
+        self.target_link = target_link
+        self.article_titles = list(internal_link_pairs.keys())
+        self.articles = internal_link_pairs
+        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+    
+    def best_match(self):
+        corpus_embeddings = self.model.encode(
+            self.article_titles, convert_to_tensor=True
+        )
+        corpus_embeddings = util.normalize_embeddings(corpus_embeddings)
+
+        query_embedding = self.model.encode(
+            [self.target_link], convert_to_tensor=True
+        )
+        query_embedding = util.normalize_embeddings(query_embedding)
+
+        hits = util.semantic_search(
+            query_embedding, corpus_embeddings, top_k=1, score_function=util.dot_score
+        )
+        top_match = hits[0][0]
+
+        best_title = self.article_titles[top_match["corpus_id"]]
+        best_link = self.articles[best_title]
+
+        return {best_title: best_link}
--- src/navigator.py
+++ src/navigator.py
@@ -1,24 +1,12 @@
 from scraper import Scraper
-import torch
-from sentence_transformers import SentenceTransformer, util
+from models import WikiModel
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-initial_url = 'https://en.wikipedia.org/wiki/Feudal'
+initial_url = "https://en.wikipedia.org/wiki/Glacial_landform"
 scraper_instance = Scraper(initial_url)
+
 links = scraper_instance.get_links()
-
 target_title = "Peasant"
-article_titles = list(links.keys())
 
-corpus_embeddings = model.encode(article_titles, convert_to_tensor=True)
-corpus_embeddings = util.normalize_embeddings(corpus_embeddings)
-
-query_embedding = model.encode([target_title], convert_to_tensor=True)
-query_embedding = util.normalize_embeddings(query_embedding)
-
-hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=1, score_function=util.dot_score)
-top_match = hits[0][0]
-
-best_title = article_titles[top_match["corpus_id"]]
-best_link = links[best_title]
+model = WikiModel(initial_url, target_title, links)
+best_match = model.best_match()
+print(best_match)
