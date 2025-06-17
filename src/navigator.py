67ea643e238149a5963a104daad34922df419e20
physimcs <styburczy.business@gmail.com>
2025-06-17 16:50:26 -0400

    Added input verifiction for initial link

--- src/navigator.py
+++ src/navigator.py
@@ -1,13 +1,16 @@
 from scraper import Scraper
 from models import WikiModel
+import wikipedia
 
-initial_url = "https://en.wikipedia.org/wiki/Glacial_landform"
-target_title = "Peasant"
+initial = wikipedia.search(input('Enter your initial article title: '))[0]
+target_title = input('Enter your target article title: ')
+
+initial_url = 'https://en.wikipedia.org/wiki/' + initial.replace(' ', '_')
 
 def navigate(current_article, target_title):
     print(f"Currently at: {current_article}")
 
-    if current_article == 'https://en.wikipedia.org/wiki/' + target_title:
+    if current_article == 'https://en.wikipedia.org/wiki/' + target_title.replace(' ', '_'):
         return "Game won!"
 
     scraper = Scraper(current_article)
@@ -18,5 +21,5 @@ def navigate(current_article, target_title):
 
     return navigate(best_match[1], target_title)
 
-result = navigate(initial_url, target_title)
-print(result)
+match = navigate(initial_url, target_title)
+print(match)
